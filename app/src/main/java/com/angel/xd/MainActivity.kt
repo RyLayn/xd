a1dc4a84cc3bb0883821c1cf33543a6c7e68e52e
AC.RyLayn <angelcabrera0272@gmail.com>
2023-02-03 15:24:04 -0500

    Actualización parte 1

--- app/src/main/java/com/angel/xd/MainActivity.kt
+++ app/src/main/java/com/angel/xd/MainActivity.kt
@@ -3,6 +3,8 @@ package com.angel.xd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val username: EditText = findViewById(R.id.user)
         val loginButton: Button = findViewById(R.id.login_button)
         loginButton.setOnClickListener {
             Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            var angel: String = "Hola mundo"
+            angel = "Hola a todos"
+            loginButton.text = angel
+            username.setText("Usuarito")
         }
+
+        val imageButton: ImageView = findViewById(R.id.imagenl)
+        imageButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Vamos a morir", Toast.LENGTH_SHORT).show()
+
+        }
+
 
     }
 }
