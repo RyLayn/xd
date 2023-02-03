291ffed00958b07a1243e80efce4756761706fc0
Giancarlo Calderón <giancarlocalderonp@gmail.com>
2023-02-03 10:34:15 -0500

    :art: Login button functionality

--- app/src/main/java/com/angel/xd/MainActivity.kt
+++ app/src/main/java/com/angel/xd/MainActivity.kt
@@ -2,10 +2,18 @@ package com.angel.xd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
