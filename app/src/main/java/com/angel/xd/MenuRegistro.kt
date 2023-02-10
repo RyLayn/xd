c98e9c1ce26c1ba7e3fb40e98b7888edd0efac73
AC_RyLayn <angelcabrera0272@gmail.com>
2023-02-10 09:55:52 -0500

    Boton Crear Cuenta regresa información

--- app/src/main/java/com/angel/xd/MenuRegistro.kt
+++ app/src/main/java/com/angel/xd/MenuRegistro.kt
@@ -3,19 +3,39 @@ package com.angel.xd
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MenuRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_registro)
+        val editPassword: EditText = findViewById(R.id.password)
+        val nomCompleto: EditText = findViewById(R.id.nombrecomp)
+        val repPassword: EditText = findViewById(R.id.repetirpassword)
+        val correoelec: EditText = findViewById(R.id.correoelec)
+
 
         val textViewInicio: TextView = findViewById(R.id.iniciocuenta)
         textViewInicio.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+
         }
+
+        val button: androidx.appcompat.widget.AppCompatButton = findViewById(R.id.botonregistro)
+        button.setOnClickListener {
+            var text: String = editPassword.text.toString()
+            var text1: String = repPassword.text.toString()
+            var text2: String = nomCompleto.text.toString()
+            var text3: String = correoelec.text.toString()
+
+            Toast.makeText(this@MenuRegistro, text2 + text3 + text + text1, Toast.LENGTH_SHORT)
+                .show()
+        }
+
+
     }
-
-
 }
